2f6c61ee87d8e736dbc1530976f04913c3f98499
Edner Paulo Severo Dias <163218158+EdnerPaulo@users.noreply.github.com>
2026-05-08 21:40:47 -0300

    Add files via upload

--- app/models/database.py
+++ app/models/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, Column , Integer, String
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv('postgresql://neondb_owner:npg_2e8oxAOlhayc@ep-fancy-mud-apjbkmcm-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
+SQLALCHEMY_DATABASE_URL = postgresql://neondb_owner:npg_2e8oxAOlhayc@ep-fancy-mud-apjbkmcm-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require
 
 if SQLALCHEMY_DATABASE_URL.startswith('postgres://'):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://","postgres://",1)
